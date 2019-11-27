a4d90a04533f6eaa8fd58330239ae4466cc06ce2
Koen de Nooij <kdnooij@users.noreply.github.com>
2019-11-27 13:51:37 +0100

    Updates

---
+++ precache-manifest.740de69c015bc4b1f2739e77c5d340a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ddc674f111fc4080180f917c8715a1",
+    "url": "/formula-manipulation/index.html"
+  },
+  {
+    "revision": "44bac8d0eb9247a92ead",
+    "url": "/formula-manipulation/static/css/main.76f8c067.chunk.css"
+  },
+  {
+    "revision": "9fb8890982831985afe5",
+    "url": "/formula-manipulation/static/js/2.f6e7926b.chunk.js"
+  },
+  {
+    "revision": "44bac8d0eb9247a92ead",
+    "url": "/formula-manipulation/static/js/main.0c4e8ad3.chunk.js"
+  },
+  {
+    "revision": "c50e5fc3ffa75472120b",
+    "url": "/formula-manipulation/static/js/runtime-main.dc325633.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/formula_manipulation/precache-manifest.65c8910269735f52ef095549a723fd1b.js"
+  "/formula-manipulation/precache-manifest.740de69c015bc4b1f2739e77c5d340a6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/formula_manipulation/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/formula-manipulation/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
